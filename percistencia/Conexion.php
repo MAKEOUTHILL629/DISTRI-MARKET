b66745464b3a0c23bc254c257ef1eadecde484ba
MAKE OUT HILL <felipeh2002@gmail.com>
2021-04-14 14:09:02 -0500

    Se agrego completamente la percistencia

--- percistencia/Conexion.php
+++ percistencia/Conexion.php
@@ -1,9 +1,27 @@
 <?php
 class Conexion{
-    private $mysql;
+    private $mysqli;
     private $resultado;
+    
+    function abrir(){
+        $this -> mysqli = new mysqli("localhost", "root", "", "distri-market");
+        //$this -> mysqli = new mysqli("localhost", "itiud_amazonas", "86Q2fyaICa", "itiud_amazonas");        
+        $this -> mysqli -> set_charset("utf8");
+    }
 
-    funcion abrir(){
-        $this -> mysql = new mysql("localhost", "root", "","")
+    function cerrar(){
+        $this -> mysqli -> close();
+    }
+    
+    function ejecutar($sentencia){
+        $this -> resultado = $this -> mysqli -> query($sentencia);
+    }
+    
+    function extraer(){
+        return $this -> resultado -> fetch_row();
+    }
+    
+    function numFilas(){
+        return ($this -> resultado != null) ? $this -> resultado -> num_rows : 0; 
     }
 }
